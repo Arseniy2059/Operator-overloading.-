85ab05315682bf956466e58cc827d9b9932a64da
Arseniy2059 <semyashulkovy64@mail.ru>
2024-09-26 22:56:05 +0300

    Update fake_math.py

--- fake_math.py
+++ fake_math.py
@@ -1,50 +1,77 @@
 class House:
+
     def __init__(self, name, number_of_floors):
         self.name = name
-        self.number_of_floors = number_of_floors # Характиристика
-
+        self.number_of_floors = number_of_floors  # Характеристика
 
     def __len__(self):
-        return self.number_of_floors # для потчета этожей
+        return self.number_of_floors  # для подсчета этажей
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors and self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors > other
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
 
     def __str__(self):
-        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}" #
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
     def __add__(self, value):
-        return self.number_of_floors + value.number_of_floors
+        if isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        elif isinstance(value, int):
+            return self
 
     def __radd__(self, value):
-        return self.number_of_floors + value.number_of_floors
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
 
 
-h1: House = House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация', 20) #
-
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
 
 print(h1)
 print(h2)
 
-print(20 == 20)
-print(10 <= 20)
-print(10 > 20)
-print(10 >= 20)
-print(10 != 20)
+print(h1 == h2) # __eq__
 
-print(len(h1))
-print(len(h2))
-self.name == other.name
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
